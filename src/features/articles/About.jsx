9154078e648e4cd8669877ad876582b7a9172003
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-01-20 01:50:33 +0330

    fix respnsive bug3

--- src/features/articles/About.jsx
+++ src/features/articles/About.jsx
@@ -2,14 +2,19 @@ import { ImMagicWand, ImProfile } from 'react-icons/im';
 import Title from '../ui/Title';
 import Typewriter from '../ui/Typewriter';
 const text = `
-A system administrator, or sysadmin, Data scientist, Systems 
-engineer from USA, California. I have rich experience
- in managing,troubleshooting, licensing, and updating
+A system administrator, or sysadmin,
+ Data scientist, Systems 
+engineer from USA, California.
+ I have rich experience
+ in managing,troubleshooting,
+  licensing, and updating
  hardware and software assets. 
- engineer A system administrator,or sysadmin,
-  Data scientist, Systems engineer from USA, California. 
-I have rich experience in managing,troubleshooting,
- licensing, and updating  hardware and software assets.
+ engineer A system administrator,
+ or sysadmin,
+  Data scientist, Systems engineer from USA,
+   California. I have rich experience
+    in managing,troubleshooting,licensing, 
+ and updating  hardware and software assets.
 
 `;
 export default function About() {
@@ -32,7 +37,7 @@ export default function About() {
       />
 
       <div className="text-[--primary-color-300]lg:px-10 px-7 xl:px-8  ">
-        <div className="font-extralightligh  pb-12 text-lg  md:text-xl xl:text-lg  2xl:text-2xl 2xl:leading-relaxed mb-60 2xl:mb-96 relative  ">
+        <div className="font-extralightligh  relative mb-60  pb-12 text-lg  md:text-xl xl:text-lg 2xl:mb-96 2xl:text-2xl 2xl:leading-relaxed  ">
           <Typewriter text={text} wordDelay={30} />
         </div>
 

4f1df0d4e202c269c7b35cccf70f6948f6842085
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-01-25 17:08:46 +0330

    loader 2

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,7 @@ const open = 'lg:!block lg:!opacity-100 animated-element';
 
 function App() {
   const [activeComponent, setActiveComponent] = useState('about');
-  const [isLoading, setIsloading] = useState(true);
 
-  function handleLoad() {
-    setIsloading(false);
-  }
 
   function showActiveComponent(component) {
     setActiveComponent(component);
@@ -31,8 +27,7 @@ function App() {
   return (
     <>
       <div className="z-[9999] flex-col overflow-x-hidden text-[--color-stone]  md:ml-7 md:flex md:flex-row md:items-start md:justify-center md:gap-5">
-       
-     { isLoading && <Loader />}
+
 
         <MouseMover />
         <MatrixBackground />
@@ -42,7 +37,7 @@ function App() {
         <main className="  md:mr-16 md:w-4/6 md:text-xl md:shadow-lg md:shadow-[var(--secondary-color-500)] lg:m-0 lg:mr-9 lg:flex lg:w-full lg:items-start lg:justify-between lg:gap-5 lg:shadow-none ">
           {/* lg:max-h-[33rem] */}
           <div className=" lg:mt-4   lg:min-h-[90vh]  lg:min-w-[38vw] xl:lg:min-w-[36vw]  2xl:min-w-[31vw]  ">
-            <HiroCard onLoad={handleLoad} />
+            <HiroCard  />
           </div>
 
           <section className="lg:my-5 lg:max-h-[93vh] lg:min-w-[27rem] lg:overflow-y-auto lg:rounded-xl  lg:border lg:border-[var(--secondary-color-500)] lg:shadow-lg  lg:shadow-[var(--secondary-color-500)] xl:mr-10   xl:min-w-80 ">
--- src/features/hiro/HiroCard.jsx
+++ src/features/hiro/HiroCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   ImCloudDownload,
   ImCompass,
@@ -7,36 +8,46 @@ import {
   ImMail,
   ImTelegram,
 } from 'react-icons/im';
-const hoverStyle = 'hover:text-[--secondary-color-500] duration-300 hover:scale-125'
+import Loader from '../ui/Loader';
+const hoverStyle =
+  'hover:text-[--secondary-color-500] duration-300 hover:scale-125';
 
-export default function HiroCard({onLoad}) {
+export default function HiroCard({ onLoad }) {
+  const [isLoading, setIsloading] = useState(true);
+
+  function handleLoad() {
+    setIsloading(false);
+  }
+
   return (
     <div className="mb-5 flex flex-col items-center justify-center rounded-b-xl bg-[var(--primary-color-800)] md:mt-2    md:gap-3 md:text-xl md:shadow-md md:shadow-[var(--secondary-color-500)]  ">
+      {isLoading || <Loader />}
       <video
-      onLoadedData={onLoad}
+        onLoadedData={handleLoad}
         autoPlay
         loop
         muted
         playsInline
         preload="auto"
-        className="my-clip-path h-96 w-full object-cover  md:h-70 md:rounded-t-lg lg:h-[40vh]    "
+        className="my-clip-path md:h-70 h-96 w-full  object-cover md:rounded-t-lg lg:h-[40vh]    "
       >
         <source src="/assets/card-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <img
-        className="z-10 -mt-28  lg:-mt-20 h-44 w-44 lg:h-28 lg:w-28 rounded-2xl border-[var(--color-black)] shadow-lg shadow-[var(--secondary-color-500)]"
+        className="z-10 -mt-28  h-44 w-44 rounded-2xl border-[var(--color-black)] shadow-lg shadow-[var(--secondary-color-500)] lg:-mt-20 lg:h-28 lg:w-28"
         src="/assets/img-3.png"
         alt=""
       />
       <h1 className="mt-5 font-serif text-2xl capitalize  tracking-widest md:text-2xl">
         Hamid Mohammadi
       </h1>
-      <h2 className="text-lg font-serif  capitalize text-[var(--secondary-color-400)]">
+      <h2 className="font-serif text-lg  capitalize text-[var(--secondary-color-400)]">
         Front-End Devloper
       </h2>
       <div className="my-3 flex w-full cursor-pointer items-center justify-center gap-5 p-3 md:text-xl  ">
-        <a className={hoverStyle}
+        <a
+          className={hoverStyle}
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.instagram.com/hamid_develop_"
@@ -44,28 +55,32 @@ export default function HiroCard({onLoad}) {
           <ImInstagram />
         </a>
 
-        <a className={hoverStyle}
+        <a
+          className={hoverStyle}
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/hamid-js"
         >
           <ImGithub />
         </a>
-        <a className={hoverStyle}
+        <a
+          className={hoverStyle}
           target="_blank"
           rel="noopener noreferrer"
           href="mailto: hamid.reactjs@gmail.com"
         >
           <ImMail />
         </a>
-        <a className={hoverStyle}
+        <a
+          className={hoverStyle}
           target="_blank"
           rel="noopener noreferrer"
           href="https://t.me/hamid_develop"
         >
           <ImTelegram />
         </a>
-        <a className={hoverStyle}
+        <a
+          className={hoverStyle}
           target="_blank"
           rel="noopener noreferrer"
           href="https://www.linkedin.com/in/hamidreza-mohamadi-a1357a249/"
@@ -75,10 +90,10 @@ export default function HiroCard({onLoad}) {
       </div>
       <div className="flex w-full items-center justify-evenly border-t border-[var(--primary-color-700)] uppercase text-[var(--color-white)] lg:text-lg">
         <a
-          className="p-4 hover:text-[--secondary-color-500] duration-300 hover:scale-110"
+          className="p-4 duration-300 hover:scale-110 hover:text-[--secondary-color-500]"
           href="/assets/hamidmohamadi.pdf"
           download="Hamid-Mohamadi-Frontend.pdf"
-          target='_blank'
+          target="_blank"
         >
           <div className="flex items-center justify-center gap-2">
             <span>download cv</span>
@@ -90,7 +105,11 @@ export default function HiroCard({onLoad}) {
         </a>
 
         <span className="h-12 border-r-[1px] border-[var(--primary-color-700)] "></span>
-        <a className="p-4 hover:text-[--secondary-color-500] duration-300 hover:scale-110" href="tel:09371355148" target='_blank'>
+        <a
+          className="p-4 duration-300 hover:scale-110 hover:text-[--secondary-color-500]"
+          href="tel:09371355148"
+          target="_blank"
+        >
           <div className="flex items-center justify-center gap-2">
             <span> contact me</span>
 
